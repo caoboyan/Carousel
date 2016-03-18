b65b31835472694e7151a9d4a48f1a87bdb5e5ce
boyancao <boyancao@macdeMacBook-Pro.local>
2016-03-18 23:20:23 +0800

    第一次提交

---
+++ carousel/carousel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  carousel
+//
+//  Created by boyancao on 16/3/18.
+//  Copyright © 2016年 boyancao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ carousel/carousel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  carousel
+//
+//  Created by boyancao on 16/3/18.
+//  Copyright © 2016年 boyancao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
